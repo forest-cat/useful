707b18f77f628b71696959719d0dfa1857bc394d
forest-cat <71219154+forest-cat@users.noreply.github.com>
2021-09-05 01:24:50 +0200

    Create is_palindrome.py

---
+++ is_palindrome.py
@@ -0,0 +1,25 @@
+#######################
+# Script by Forestcat #
+#######################
+
+"""
+This Script checks if a String is palindrome and returns True or False.
+At the Bottom you have an example of the Function
+Have fun and enjoy!
+"""
+
+
+
+def is_palindrome(string):
+    for pos,i in enumerate(reversed(string)):
+        if pos == 0:
+            reversed_string = i
+        else:
+            reversed_string = reversed_string + i
+
+    if string == reversed_string:
+        return True
+    else:
+        return False
+
+print(is_palindrome("racecar"))
